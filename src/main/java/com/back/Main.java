f56a0119d29b19df54d5bd5e3cc859b35c301f3a
DoubleSang <hdsa1564@gmail.com>
2026-04-21 14:31:11 +0900

    work

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -8,14 +8,27 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        int lastId = 0;
+
         while (true) {
             System.out.print("명령) ");
 
             String cmd = scanner.nextLine().trim();
 
-            if (cmd.equals("exit")) {
+            if (cmd.equals("종료")) {
                 System.out.println("종료합니다.");
                 break;
+            }
+            else if (cmd.equals("등록")) {
+                System.out.print("명언: ");
+                String wiseSayingContent = scanner.nextLine().trim();
+                System.out.print("작가: ");
+                String wiseSayingAuthor = scanner.nextLine().trim();
+
+                int no = ++lastId;
+
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(no));
+
             }
         }
 
